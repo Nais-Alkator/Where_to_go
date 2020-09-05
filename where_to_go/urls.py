583aac36d5b65015b4633c5fe7eca42ca9b82633
Alkator-Nori <nais000k@gmail.com>
2020-09-05 22:02:04 +0300

    create api endpoint

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index),
+    path("places/<str:place_id>/", views.show_title),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from places.models import Place
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -27,3 +29,6 @@ def index(request):
     return render(request, "index.html", context=data)
 
 
+def show_title(request, place_id):
+    place = get_object_or_404(Place, place_id=place_id)
+    return HttpResponse(place.title)
