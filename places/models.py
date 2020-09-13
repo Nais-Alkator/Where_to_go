acc786118f85177c96aca82ec97580eae5f678ea
Alkator-Nori <nais000k@gmail.com>
2020-09-13 11:02:49 +0300

    added WYSIWYG

--- places/models.py
+++ places/models.py
@@ -1,22 +1,22 @@
 from django.db import models
-from django.contrib import admin
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
     description_short = models.TextField(blank=True)
-    description_long = models.TextField(blank=True)
+    description_long = HTMLField()
     longitude = models.FloatField()
     latitude = models.FloatField()
+    place_id = models.CharField(max_length=20)
     
-
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
     image = models.ImageField(upload_to="places_images", blank=True)
-    place_image = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="places_image")
+    place_image = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="place_image")
     position = models.PositiveIntegerField(default=0, blank=False, null=False)
 
     def __str__(self):
