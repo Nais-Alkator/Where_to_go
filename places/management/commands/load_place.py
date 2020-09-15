7d2dd1a082a58225904a8264755f8a7332c06673
Alkator-Nori <nais000k@gmail.com>
2020-09-15 23:07:04 +0300

    add load_place management command

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+from places.models import Place, Image
+import requests
+from django.core.files.base import ContentFile
+
+class Command(BaseCommand):
+    help = 'Загружаем локации и картинки в БД'
+
+
+    def add_arguments(self, parser):
+        parser.add_argument('url')
+
+
+    def handle(self, *args, **options):
+        url = options['url']
+        response = requests.get(url)
+        response.raise_for_Status()
+        response = response.json()
+
+        place = Place.objects.get_or_create(title=response['title'], 
+            description_short=response['description_short'],
+            description_long=response['description_long'],
+            longitude=response['coordinates']['lng'],
+            latitude=response['coordinates']['lat'])
+
+        for image_link in response['imgs']:
+            response = requests.get(image_link)
+            imagefile = ContentFile(response.content)
+            filename = image_link.split('/')[-1]
+
+            image = Image.objects.create(place=place)
+            image.file.save(filename, imagefile, save=True)
