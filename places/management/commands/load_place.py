fea7fc69c5a371be1afe35204b3f2c106143d28c
Alkator-Nori <nais000k@gmail.com>
2020-09-20 19:48:20 +0300

    update

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -3,30 +3,32 @@ from places.models import Place, Image
 import requests
 from django.core.files.base import ContentFile
 
+
 class Command(BaseCommand):
     help = 'Загружаем локации и картинки в БД'
 
 
     def add_arguments(self, parser):
-        parser.add_argument('url')
+        parser.add_argument('url', type=str)
 
 
     def handle(self, *args, **options):
         url = options['url']
         response = requests.get(url)
-        response.raise_for_Status()
+        response.raise_for_status()
         response = response.json()
 
-        place = Place.objects.get_or_create(title=response['title'], 
+        place = Place.objects.create(title=response['title'], 
             description_short=response['description_short'],
             description_long=response['description_long'],
             longitude=response['coordinates']['lng'],
-            latitude=response['coordinates']['lat'])
+            latitude=response['coordinates']['lat'],
+            place_id=response['title'])
 
         for image_link in response['imgs']:
             response = requests.get(image_link)
             imagefile = ContentFile(response.content)
             filename = image_link.split('/')[-1]
 
-            image = Image.objects.create(place=place)
-            image.file.save(filename, imagefile, save=True)
+            image = Image.objects.create(place_image=place)
+            image.image.save(filename, imagefile, save=True)
