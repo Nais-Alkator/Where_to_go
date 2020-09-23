9f27c29077b3506a69b2ad921b5f0552804bee7a
Alkator-Nori <nais000k@gmail.com>
2020-09-23 20:07:57 +0300

    Hided secret_key

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "%90-2u%4ll9m+7k72lg717##m9s74#@en=k%-26z4hju1drcb3"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
