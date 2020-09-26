d875e23f65a4128da41888e845011ce1d6813988
Alkator-Nori <nais000k@gmail.com>
2020-09-26 18:06:54 +0300

    hided debug setting, added verbose name for models

--- places/models.py
+++ places/models.py
@@ -3,21 +3,21 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    description_short = models.TextField(blank=True)
-    description_long = HTMLField()
-    longitude = models.FloatField()
-    latitude = models.FloatField()
-    place_id = models.CharField(max_length=20)
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    description_short = models.TextField(blank=True, verbose_name="Краткое описание")
+    description_long = HTMLField(verbose_name="Подробное описание")
+    longitude = models.FloatField(verbose_name="Долгота")
+    latitude = models.FloatField(verbose_name="Широта")
+    place_id = models.CharField(max_length=20, verbose_name="Уникальный заголовок")
     
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to="places_images", blank=True)
-    place_image = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="place_image")
-    position = models.PositiveIntegerField(default=0, blank=False, null=False)
+    image = models.ImageField(upload_to="places_images", blank=True, verbose_name="Файл изображения")
+    place_image = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="place_image", verbose_name="Изображение")
+    position = models.PositiveIntegerField(default=0, blank=False, null=False, verbose_name="Порядковый номер")
 
     def __str__(self):
         return f"{self.position} {self.place_image}"
